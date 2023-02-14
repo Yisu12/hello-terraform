d6ef40e03496bbc3a589b7967b9de8cb0d34f7b4
Yisu <mchjesus6@gmail.com>
2023-02-14 23:43:27 +0100

    Soluciono la linea de security_group

--- main.tf
+++ main.tf
@@ -17,15 +17,10 @@ resource "aws_instance" "app_server" {
   ami                     = "ami-0b752bf1df193a6c4"
   instance_type           = "t2.micro"
   key_name                = "clave-lucatic"
-  #vpc_security_group_ids = "sg-0562854c9b1329776"
+  vpc_security_group_ids = ["sg-0562854c9b1329776"]
 
   tags = {
     Name = "ExampleServerInstance"
   }
 }
 
-resource "aws_security_group" "sg" {
-  tags = {
-    type = "terraform-test-security-group"
-  }
-}
